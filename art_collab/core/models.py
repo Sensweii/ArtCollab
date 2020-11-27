a7abf770b4d5220783ba598ca0d5db92473944d4
Sensweii <alen.dg.balbuena@gmail.com>
2020-11-27 16:38:07 +0800

    Move TimeStampModel to core app.

--- art_collab/core/models.py
+++ art_collab/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampModel(models.Model):
+    """
+    Base model for setting time stamps. Inherit this class when time
+    tracking is desired.
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
--- art_collab/users/models.py
+++ art_collab/users/models.py
@@ -2,20 +2,8 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
+from core.models import TimeStampModel
 from .managers import UserManager
-
-
-class TimeStampModel(models.Model):
-    """
-    Base model for setting time stamps. Inherit this class when time
-    tracking is desired.
-    """
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(blank=True, null=True)
-
-    class Meta:
-        abstract = True
 
 
 class User(AbstractBaseUser, PermissionsMixin, TimeStampModel):
