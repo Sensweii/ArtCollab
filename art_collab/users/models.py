eb520ed402813f5e44ef8d93540ba0eca4de2d9d
Sensweii <alen.dg.balbuena@gmail.com>
2020-11-27 16:31:52 +0800

    Add users app and widget_tweaks package.

---
+++ art_collab/users/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.db import models
+
+from .managers import UserManager
+
+
+class TimeStampModel(models.Model):
+    """
+    Base model for setting time stamps. Inherit this class when time
+    tracking is desired.
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(AbstractBaseUser, PermissionsMixin, TimeStampModel):
+    """Model to override Django's User model."""
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=128, blank=True)
+    last_name = models.CharField(max_length=128, blank=True)
+    is_active = models.BooleanField(default=True)
+    verified = models.BooleanField(
+        default=False,
+        help_text='Set `True` for users that verified email.')
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def update_password(self, password):
+        """Method to ensure password hashing on update."""
+        self.set_password(password)
+        self.save()
