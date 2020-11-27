6d6b3f48a1158471847724f1685b4fc81bc74be5
Sensweii <alen.dg.balbuena@gmail.com>
2020-11-27 16:58:26 +0800

    Add home view.

--- art_collab/core/views.py
+++ art_collab/core/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    """View for home page."""
+    template_name = 'homepage.html'
