c6f7b656aaaf848ac2d9ed77a7e1e2f41509c779
Sensweii <alen.dg.balbuena@gmail.com>
2020-11-30 11:14:23 +0800

    Add logo model and migration.

--- art_collab/logos/models.py
+++ art_collab/logos/models.py
@@ -3,9 +3,10 @@ from django.db import models
 from core.models import TimeStampModel
 from users.models import User
 
-class Logos(TimeStampModel):
+class Logo(TimeStampModel):
     """Model for logo objects."""
 
-    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
     name = models.CharField(max_length=256)
     image = models.ImageField(blank=True)
+    image_link = models.CharField(max_length=512, blank=True)
