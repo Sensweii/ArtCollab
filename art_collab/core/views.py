fcd0655e44cce130006fa16a50337a34ae548bda
Sensweii <alen.dg.balbuena@gmail.com>
2020-12-03 13:21:38 +0800

    Initial commit for passing context data to be consumed by react.

--- art_collab/core/views.py
+++ art_collab/core/views.py
@@ -1,6 +1,15 @@
 from django.views.generic import TemplateView
 
+from logos.models import Logo
+
 
 class HomeView(TemplateView):
     """View for home page."""
     template_name = 'homepage.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        logo_queryset = Logo.objects.all()
+        context['logos'] = list(
+            logo_queryset.values('id', 'author_id', 'name', 'image'))
+        return context
